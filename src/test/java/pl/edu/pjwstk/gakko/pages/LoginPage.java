e58829aa35c9f688776c1fb20a18ce29c418a043
Mateusz Lubowiecki <matlubowiecki@gmail.com>
2024-05-14 14:29:32 +0200

    add Logger to LoginPage, add logs to logIn method

--- src/test/java/pl/edu/pjwstk/gakko/pages/LoginPage.java
+++ src/test/java/pl/edu/pjwstk/gakko/pages/LoginPage.java
@@ -1,5 +1,7 @@
 package pl.edu.pjwstk.gakko.pages;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -17,14 +19,18 @@ public class LoginPage {
 
     private WebDriver driver;
 
+    private static final Logger logger = LogManager.getLogger();
+
     public LoginPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
         this.driver = driver;
     }
 
     public void logIn(String login, String password) {
+        logger.info("entering login and password");
         userNameInput.sendKeys(login);
         passwordInput.sendKeys(password);
         submitButton.click();
+        logger.info("entering login and password done");
     }
 }
