a59d3cc18e111543169c8293cfbfb6c4e0acad28
Mateusz Lubowiecki <matlubowiecki@gmail.com>
2024-05-14 14:21:26 +0200

    add ExtentReports to BaseTest

--- src/test/java/pl/edu/pjwstk/gakko/tests/BaseTest.java
+++ src/test/java/pl/edu/pjwstk/gakko/tests/BaseTest.java
@@ -1,10 +1,14 @@
 package pl.edu.pjwstk.gakko.tests;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import pl.edu.pjwstk.gakko.pages.LoginPage;
 import pl.edu.pjwstk.gakko.utils.PropertiesLoader;
 
@@ -14,8 +18,23 @@ import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
 
-
     public WebDriver driver;
+
+    protected static ExtentSparkReporter sparkReporter;
+
+    protected static ExtentReports extentReports;
+
+    @BeforeSuite
+    public void beforeSuite() {
+        sparkReporter = new ExtentSparkReporter("index.html");
+        extentReports = new ExtentReports();
+        extentReports.attachReporter(sparkReporter);
+    }
+
+    @AfterSuite
+    public void afterSuite() {
+        extentReports.flush();
+    }
 
     @BeforeMethod
     public void setup() throws IOException {
