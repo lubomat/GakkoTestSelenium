623b71159e79cbeb2f7b90c25ae61fc24a537177
Mateusz Lubowiecki <matlubowiecki@gmail.com>
2024-05-14 12:25:16 +0200

    edit BaseTest

--- src/test/java/pl/edu/pjwstk/gakko/tests/BaseTest.java
+++ src/test/java/pl/edu/pjwstk/gakko/tests/BaseTest.java
@@ -5,8 +5,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import pl.edu.pjwstk.gakko.pages.LoginPage;
+import pl.edu.pjwstk.gakko.utils.PropertiesLoader;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
@@ -18,10 +21,18 @@ public class BaseTest {
     public void setup() throws IOException {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-//        driver = DriverFactory.getDriver();
         driver.manage().timeouts().implicitlyWait(2L, TimeUnit.SECONDS);
         driver.get("https://gakko.pjwstk.edu.pl/edux/7984/");
         driver.manage().window().maximize();
+
+        PropertiesLoader testUtils = new PropertiesLoader();
+        Map<String, String> loginData = testUtils.readLoginDataFromFile("C:/Users/matlu/IdeaProjects/gakkoTest/src/config/login_credentials.txt");
+        String login = loginData.get("login");
+        String password = loginData.get("password");
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login(login,password);
+
     }
 
     @AfterMethod
