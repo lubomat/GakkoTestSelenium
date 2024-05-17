60145a27bb0aae1bf815fdd9794e28d26e5b926b
Mateusz Lubowiecki <matlubowiecki@gmail.com>
2024-05-17 18:23:33 +0200

    add logs and screenshots to addGradeWithExistingNameTest

--- src/test/java/pl/edu/pjwstk/gakko/tests/GradesTest.java
+++ src/test/java/pl/edu/pjwstk/gakko/tests/GradesTest.java
@@ -31,12 +31,16 @@ public class GradesTest extends BaseTest{
     }
 
     @Test
-    public void addGradeWithExistingNameTest() {
+    public void addGradeWithExistingNameTest() throws IOException {
 
         ExtentTest test = extentReports.createTest("Creating New Grade With Existing Name Test");
         GradesPage gradesPage = new GradesPage(driver);
+        test.log(Status.PASS,"Entering grades page", SeleniumHelper.getScreenshot(driver));
         gradesPage.enterGradePage();
+        test.log(Status.PASS,"Entering grades page done", SeleniumHelper.getScreenshot(driver));
+        test.log(Status.PASS,"Creating new grade with existing name", SeleniumHelper.getScreenshot(driver));
         gradesPage.addNewGrade("Nazwa Testowa");
+        test.log(Status.PASS,"Creating new grade with existing name Done", SeleniumHelper.getScreenshot(driver));
 
         Assert.assertTrue(gradesPage.gradesAddMessage.isDisplayed());
         Assert.assertEquals(gradesPage.getMessage(),"Ocena o identycznych parametrach już istnieje");
