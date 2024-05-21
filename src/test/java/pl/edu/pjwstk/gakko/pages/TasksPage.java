8fa3c5763f58b8f4792097dca12da92ba3146f1c
Mateusz Lubowiecki <matlubowiecki@gmail.com>
2024-05-21 13:04:25 +0200

    create addTaskTest

--- src/test/java/pl/edu/pjwstk/gakko/pages/TasksPage.java
+++ src/test/java/pl/edu/pjwstk/gakko/pages/TasksPage.java
@@ -1,4 +1,27 @@
 package pl.edu.pjwstk.gakko.pages;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class TasksPage {
+
+    @FindBy(xpath = "//span[text()='Zadania']")
+    private WebElement tasksButton;
+
+    private WebDriver driver;
+
+    public static final Logger logger = LogManager.getLogger();
+
+    public TasksPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    public void enterTasksModule() {
+        tasksButton.click();
+    }
 }
--- src/test/java/pl/edu/pjwstk/gakko/tests/TasksTest.java
+++ src/test/java/pl/edu/pjwstk/gakko/tests/TasksTest.java
@@ -1,11 +1,14 @@
 package pl.edu.pjwstk.gakko.tests;
 
 import org.testng.annotations.Test;
+import pl.edu.pjwstk.gakko.pages.TasksPage;
 
 public class TasksTest extends BaseTest{
 
     @Test
-    public void createTaskTest() {
+    public void addTaskTest() {
+        TasksPage tasksPage = new TasksPage(driver);
+        tasksPage.enterTasksModule();
 
     }
 }
