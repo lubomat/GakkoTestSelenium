5dadd18e9f704146202ba65c6a825d3bf2e6d569
Mateusz Lubowiecki <matlubowiecki@gmail.com>
2024-06-14 13:55:23 +0200

    crete addEmptyLessonTest

---
+++ src/test/java/pl/edu/pjwstk/gakko/pages/LessonsPage.java
@@ -0,0 +1,65 @@
+package pl.edu.pjwstk.gakko.pages;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LessonsPage {
+
+    @FindBy(xpath = "//a[@href='/edux/7984/lessons']")
+    private WebElement lessonsModuleButton;
+
+    @FindBy(className = "flaticon2-plus")
+    private WebElement addLessonButton;
+
+    @FindBy(xpath = "//a[@href='/edux/7984/lessons/upsert']")
+    private WebElement directlyInCourseButton;
+
+    @FindBy(id = "Lesson_Name")
+    private WebElement nameInput;
+
+    @FindBy(id = "Lesson_PublicationDate")
+    private WebElement dateInput;
+
+    @FindBy(css = "button.mr-2.btn.btn-primary")
+    private WebElement saveButton;
+
+    @FindBy(className = "flaticon2-plus")
+    private WebElement addChapterButton;
+
+    private WebDriver driver;
+
+    private static final Logger logger = LogManager.getLogger();
+
+    public LessonsPage(WebDriver driver) {
+        PageFactory.initElements(driver,this);
+        this.driver = driver;
+    }
+
+    public void enterLessonsModule() {
+        lessonsModuleButton.click();
+    }
+
+    public void addEmptyLesson(String name, String date) throws InterruptedException {
+        logger.info("Adding lesson");
+        logger.info("Clicking add lesson button");
+        addLessonButton.click();
+        directlyInCourseButton.click();
+        logger.info("Clicking add lesson button done");
+        logger.info("Setting name");
+        nameInput.sendKeys(name);
+        logger.info("Setting name done");
+        logger.info("Clearing and seting date");
+        dateInput.clear();
+        dateInput.sendKeys(date);
+        logger.info("Clearing and seting date done");
+        logger.info("Clicking save");
+        saveButton.click();
+        logger.info("Clicking save done");
+    }
+
+
+}
---
+++ src/test/java/pl/edu/pjwstk/gakko/tests/LessonsTest.java
@@ -0,0 +1,17 @@
+package pl.edu.pjwstk.gakko.tests;
+
+import org.testng.annotations.Test;
+import pl.edu.pjwstk.gakko.pages.LessonsPage;
+
+public class LessonsTest extends BaseTest {
+
+    @Test
+    public void addEmptyLessonTest() throws InterruptedException {
+        LessonsPage lessonsPage = new LessonsPage(driver);
+        lessonsPage.enterLessonsModule();
+        lessonsPage.addEmptyLesson("Test","2025-09-13 13:00");
+    }
+
+
+
+}
